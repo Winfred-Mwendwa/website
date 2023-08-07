c7d238efab9c1f22d19ba70b7d872220a1d6119c
winfred mwendwa <mwendwawinfred8@gmail.com>
2023-08-07 22:45:02 +0300

    generate starter components

--- website-home/src/app/app.module.ts
+++ website-home/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      HeroComponent,
+      HeaderComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule
   ],
